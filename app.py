470cd1a19b156f925d05c813bd873c5b66135732
wikyprash <wikyprash@gmail.com>
2020-08-10 19:51:20 +0530

    cross fingures

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import request, render_template
-import json, os
 from automation import Automate
 
 app = Flask(__name__)
@@ -8,15 +7,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    x = Automate('163g1a0505').start()
-    return x
-    # if request.method == 'POST':
-    #     rollno = request.form.get('rollno')
-    #     rollno = rollno.upper()
-    #     print(rollno)
-    #     data = Automate(rollno).start()
-    #     return render_template('res.html', data = data)
-    # return render_template('base.html')
+    if request.method == 'POST':
+        rollno = request.form.get('rollno')
+        rollno = rollno.upper()
+        data = Automate(rollno).start()
+        return render_template('res.html', data = data)
+    return render_template('base.html')
 
 
 if __name__ == "__main__":
