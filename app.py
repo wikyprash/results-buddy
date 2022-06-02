4b8d5cfd2f9205d42573ac720c07c6b74b755f3b
prakash <wikyprash@gmail.com>
2022-06-02 22:29:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def home():
         rollno = request.form.get('rollno')
         rollno = rollno.upper()
         data = requests.get("https://jntua-results-api.herokuapp.com/allAttemptedResults?htno=183g1a0501").text
+        data = json.loads(data)
         return render_template('res.html', data = data)
     return render_template('base.html')
 
